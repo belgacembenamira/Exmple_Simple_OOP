c78ca4f6d486abfc64b2127be8487d7ee0e48f1c
belgacem <benamierabelgacem@gmail.com>
2023-11-17 18:37:25 +0100

    fix:erreur in polymerphisme

--- Main.java
+++ Main.java
@@ -2,20 +2,21 @@ public class Main {
 
   public static void main(String[] args) {
     Voiture v = new Voiture("BMW", 10);
-    // System.out.print(v.toString());
-    // Voiture v2 =new Voiture("m", 10000000);
-    // System.out.print(v2.toString());
-    // v2.setName("iheb");
-    //  System.out.print(v2.toString());
-    //  FastVoiture f= new FastVoiture("fast1", 100,120);
-    //  System.out.println(f.toString());
-    //  System.out.println(f.toString("fast1"));
-    //  System.out.println(f.toString("fast",100));
-    //  f.f_one("fast1");
-    // //  v.f_one("BMW");
-    //  v.f_three("BMW");
-     Voiture V2 =new Mouter ("Voitrure-moter", 10) ;
-    ((Mouter) V2).Affichage();
+    System.out.print(v.toString());
+    Voiture v2 =new Voiture("m", 10000000);
+    System.out.print(v2.toString());
+    v2.setName("iheb");
+     System.out.print(v2.toString());
+     FastVoiture f= new FastVoiture("fast1", 100,120);
+     System.out.println(f.toString());
+     System.out.println(f.toString("fast1"));
+     System.out.println(f.toString("fast",100));
+     f.f_one("fast1");
+    //  v.f_one("BMW");
+     v.f_three("BMW");
+     Voiture V2 =new Mouter ("Voitrure-moter", 10, null) ;
+     System.out.println(((Mouter) V2).Affichage());
+    
     
 
   }
--- Mouter.java
+++ Mouter.java
@@ -1,40 +1,35 @@
-public class Mouter {
-private String Mouter_name;
-private double Mouter_price;
-   
+public class Mouter extends Voiture {
 
-    public Mouter(String Mouter_name, double Mouter_price) {
-        this.Mouter_name = Mouter_name;
-        this.Mouter_price = Mouter_price;
-    }
+  public Mouter(String name, double price, String Mouter_name) {
+    super(name, price);
+  }
 
-  
-    public String getMouter_name() {
-        return Mouter_name;
-    }
+  public String getMouter_name() {
+    return this.name;
+  }
 
-    
-    public void setMouter_name(String Mouter_name) {
-        this.Mouter_name = Mouter_name;
-    }
+  public void setMouter_name(String Mouter_name) {
+    this.name = Mouter_name;
+  }
 
-   
-    public double getMouter_price() {
-        return Mouter_price;
-    }
+  public double getMouter_price() {
+    return this.price;
+  }
 
+  public void setMouter_price(double Mouter_price) {
+    this.price = Mouter_price;
+  }
 
-    public void setMouter_price(double Mouter_price) {
-        this.Mouter_price = Mouter_price;
-    }
-
-
-
-    public String Affichage() {
-        return "{" +
-            " Mouter_name='" + getMouter_name() + "'" +
-            ", Mouter_price='" + getMouter_price() + "'" +
-            "}";
-    }
-    
+  public String Affichage() {
+    return (
+      "{" +
+      " Mouter_name='" +
+      getMouter_name() +
+      "'" +
+      ", Mouter_price='" +
+      getMouter_price() +
+      "'" +
+      "}"
+    );
+  }
 }
